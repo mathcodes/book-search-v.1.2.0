696b4d553a751c75f75f6ac12a949a236e5543cc
mathcodes <jonpchristie@gmail.com>
2022-11-01 08:22:00 -0910

    oct

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -11,8 +11,8 @@ const bookSeed = [
       title: "The Hunger Games",
       author: "Suzanne Collins",
       description: "Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
-      image: "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-      link: "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
+      image: "https://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+      link: "https://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
     },
   ];
 

f3e65eca23076bf0278e2f85b96ca97b2551e55e
mathcodes <jonpchristie@gmail.com>
2022-11-08 08:22:00 -0910

    oct

---
+++ client/build/precache-manifest.8259e5aab3a35e48a7032dc11b781d97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdc74cf717d53ddd494c",
+    "url": "/static/css/main.d012e149.chunk.css"
+  },
+  {
+    "revision": "fdc74cf717d53ddd494c",
+    "url": "/static/js/main.9f83325c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "933356587580efd253b0",
+    "url": "/static/js/2.24b5b855.chunk.js"
+  },
+  {
+    "revision": "017fbe7c2901f1057c20132f95bd517c",
+    "url": "/index.html"
+  }
+];
